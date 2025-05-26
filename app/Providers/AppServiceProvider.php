ebc0e69cb387f0e1e35519842d19d6029d54ef85
DavinMetoti <DavinMetotii@gmail.com>
2025-05-26 15:25:38 +0700

    update config laravel telescope

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,8 +16,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->environment('production')) {
-            Telescope::night();
+        if ($this->app->environment('production') && class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
         }
     }
 
