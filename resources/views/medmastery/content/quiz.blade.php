9cbbed8f296c3aed6e778450c9996890bb2bd11b
davinmetoti <110443558+DavinMetoti@users.noreply.github.com>
2025-12-06 11:46:26 +0700

    Comment out answer section in quiz interface for future reference

--- resources/views/medmastery/content/quiz.blade.php
+++ resources/views/medmastery/content/quiz.blade.php
@@ -1080,7 +1080,7 @@
                             {!! $question->question_text !!}
                         </div>
                         
-                        <div class="answer-section">
+                        <!-- <div class="answer-section">
                             <label for="answer_{{ $question->id }}" class="form-label">
                                 <i class="fas fa-edit"></i>
                                 Jawaban Anda:
@@ -1093,12 +1093,11 @@
                                 data-question-id="{{ $question->id }}"
                                 data-question-index="{{ $index }}"
                                 rows="5"
-                                required
                             ></textarea>
                             <div class="answer-counter">
                                 <span id="counter_{{ $question->id }}">0 karakter</span>
                             </div>
-                        </div>
+                        </div> -->
                         
                         <!-- Show Explanation Button -->
                         @if($question->explanation_pdf_path)
